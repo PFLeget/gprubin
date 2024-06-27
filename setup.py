7afa47a3bcf44e9c343fb832fef60d216576615b
Pierre-Francois Leget <leget@astro.princeton.edu>
2024-06-27 12:43:18 -0400

    add the right link for gprubin

--- setup.py
+++ setup.py
@@ -17,47 +17,41 @@ print("Python version = ", sys.version)
 with open("README.rst") as f:
     long_description = f.read()
 
-# Read in the version from treegp/_version.py
+# Read in the version from gprubin/_version.py
 # cf. http://stackoverflow.com/questions/458550/standard-way-to-embed-version-into-python-package
-version_file = os.path.join("treegp", "_version.py")
+version_file = os.path.join("gprubin", "_version.py")
 with open(version_file, "rt") as f:
     VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
     txt = f.read()
     mo = re.search(VSRE, txt, re.M)
     if mo:
-        treegp_version = mo.group(1)
+        gprubin_version = mo.group(1)
     else:
         raise RuntimeError("Unable to find version string in %s." % (version_file,))
-print("treegp version is %s" % (treegp_version))
+print("gprubin version is %s" % (gprubin_version))
 
 # Package name
-name = "treegp"
+name = "gprubin"
 
 # Packages
 packages = find_packages()
 
 # Scripts (none yet, but if we make any, they will live in scripts/)
-scripts = glob.glob("scripts/*.py")
+scripts = []
 
 # Dependencies
 dependencies = [
     "numpy",
     "scipy",
-    "treecorr>=4.2",
-    "fitsio>=0.9.12",
-    "scikit-learn>=0.18",
+    "treegp>=1.2.0",
+    "jax",
 ]
-if sys.version >= "3.0":
-    dependencies += ["iminuit>2"]
-else:
-    # iminuit 1.4 fails on python 2.7
-    dependencies += ["iminuit>=1.3,<1.4"]
 
 package_data = {}
 
 setup(
     name=name,
-    description="treegp",
+    description="gprubin",
     long_description=long_description,
     license="BSD License",
     classifiers=[
@@ -65,11 +59,11 @@ setup(
         "Intended Audience :: Science/Research",
     ],
     author="PFLeget",
-    url="https://github.com/PFLeget/treegp",
-    download_url="https://github.com/PFLeget/treegp/releases/tag/v%s.zip"
-    % treegp_version,
+    url="https://github.com/PFLeget/gprubin",
+    download_url="https://github.com/PFLeget/gprubin/releases/tag/v%s.zip"
+    % gprubin_version,
     install_requires=dependencies,
-    version=treegp_version,
+    version=gprubin_version,
     packages=packages,
     scripts=scripts,
 )
